9b39ddb9d943aa9a08b8ae025c6bbdcd6fa79bcf
apachertr180navneeet <navneet@brenin.co>
2025-12-31 10:39:41 +0530

    inital commit

---
+++ resources/views/admin/layouts/elements/left_sidebar.blade.php
@@ -0,0 +1,23 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+	<div class="app-brand demo">
+		<a href="{{route('admin.dashboard')}}" class="app-brand-link">
+			<span class="app-brand-text demo menu-text fw-bold ms-2 text-capitalize">{{ config('app.name') }} Admin</span>
+		</a>
+
+		<a href="javascript:void(0);"
+			class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+			<i class="bx bx-chevron-left bx-sm align-middle"></i>
+		</a>
+	</div>
+
+	<div class="menu-inner-shadow"></div>
+
+	<ul class="menu-inner py-1">
+		<li class="menu-item {{ request()->is('admin/dashboard') ? 'active' : ''}}">
+			<a href="{{route('admin.dashboard')}}" class="menu-link">
+				<i class="menu-icon tf-icons bx bx-home-circle"></i>
+				<div data-i18n="Dashboard">Dashboard</div>
+			</a>
+		</li>
+	</ul>
+</aside>
